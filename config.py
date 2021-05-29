b6a735babedb0c58cd550b4a0ac14c7abf4135db
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-29 16:46:42 +0300

    minor

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BATCH_SIZE = 8
 NUMBER_CLASSES = 2
 INPUT_SHAPE_IMAGE = (256, 256, 3)
 PROPORTION_TEST_IMAGES = 0.2
-EPOCHS = 1000
+EPOCHS = 150
 AUGMENTATION_DATA = True
 
 DATA_PATH = 'data'
@@ -16,7 +16,7 @@ IMAGES_PATH = os.path.join(DATA_PATH, 'images')
 MASKS_PATH = os.path.join(DATA_PATH, 'masks')
 
 LEARNING_RATE = 0.0001
-BACKBONE = 'mobilenetv2'
+BACKBONE = 'resnet18'
 ENCODER_WEIGHTS = 'imagenet'
 ACTIVATION = 'softmax'
 NAME_MODEL = 'Unet'
