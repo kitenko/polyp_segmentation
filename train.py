188c35966740a9095044b08e97858e0c4ee90c47
андрей китенко <genialniy.kitenko@gmail.com>
2021-06-02 12:18:52 +0300

    changed

--- train.py
+++ train.py
@@ -1,30 +1,17 @@
 import os
 import argparse
 from typing import Tuple
+from datetime import datetime
 
 import tensorflow as tf
 import segmentation_models as sm
 from tensorflow.keras.callbacks import EarlyStopping
 
-from config import (JSON_FILE_PATH, EPOCHS, SAVE_CURRENT_MODEL, TENSORBOARD_LOGS, LEARNING_RATE, MODELS_DATA,
-                    SAVE_MODELS, SAVE_CURRENT_TENSORBOARD_LOGS, FULL_NAME_MODEL, INPUT_SHAPE_IMAGE, SAVE_CURRENT_LOGS)
-from models import build_model
-from data_generator import DataGenerator
-from logcallback import LogCallback
-
-
-def parse_args() -> argparse.Namespace:
-    """
-    Parsing command line arguments with argparse.
-    """
-    parser = argparse.ArgumentParser('script for model testing.')
-    parser.add_argument('--train', default=False, action='store_true',
-                        help='if you use this flag, then the training will be performed automatically with input shape'
-                             '(256*256*3))')
-    parser.add_argument('--train_dif_shape', default=False, action='store_true',
-                        help='if you use this flag, then the training will be performed automatically with a different '
-                             'input shape from (256*256*3) before (512*512*3)')
-    return parser.parse_args()
+from config import (JSON_FILE_PATH, EPOCHS, LEARNING_RATE, SAVE_MODELS, INPUT_SHAPE_IMAGE, MODEL_NAME,
+                    BACKBONE, LOGS)
+from src import build_model
+from src import DataGenerator
+from src import LogCallback
 
 
 def train(dataset_path_json: str, input_shape_image: Tuple[int, int, int] = INPUT_SHAPE_IMAGE) -> None:
@@ -34,9 +21,15 @@ def train(dataset_path_json: str, input_shape_image: Tuple[int, int, int] = INPU
     :param dataset_path_json: path to json file.
     :param input_shape_image: this is image shape (height, width, channels).
     """
+    date_time_for_save = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    save_path = os.path.join(args.models_data, '{}_{}_{}_shape-{}'.format(MODEL_NAME, BACKBONE, date_time_for_save,
+                                                                          str(input_shape_image[0]) + '-' +
+                                                                          str(input_shape_image[1])))
+    save_current_logs = os.path.join(save_path, LOGS)
+    save_current_model = os.path.join(save_path, SAVE_MODELS)
+
     # create dirs
-    for p in [TENSORBOARD_LOGS, MODELS_DATA, SAVE_MODELS, SAVE_CURRENT_MODEL, SAVE_CURRENT_TENSORBOARD_LOGS,
-              SAVE_CURRENT_LOGS + '_' + str(INPUT_SHAPE_IMAGE)]:
+    for p in [save_path, save_current_model, save_current_logs]:
         os.makedirs(p, exist_ok=True)
 
     train_data_gen = DataGenerator(json_path=dataset_path_json, is_train=True, image_shape=input_shape_image)
@@ -49,8 +42,8 @@ def train(dataset_path_json: str, input_shape_image: Tuple[int, int, int] = INPU
                            sm.metrics.f1_score]
                   )
     model.summary()
-    early = EarlyStopping(monitor='loss', min_delta=0, patience=50, verbose=1, mode='auto')
-    checkpoint_filepath = os.path.join(SAVE_CURRENT_MODEL, FULL_NAME_MODEL + '_' + str(input_shape_image) + '.h5')
+    early = EarlyStopping(monitor='loss', min_delta=0, patience=10, verbose=1, mode='auto')
+    checkpoint_filepath = os.path.join(save_current_model, MODEL_NAME + BACKBONE + '.h5')
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
         monitor='iou_score',
@@ -58,24 +51,46 @@ def train(dataset_path_json: str, input_shape_image: Tuple[int, int, int] = INPU
         save_best_only=True
     )
 
-    tensor_board = tf.keras.callbacks.TensorBoard(SAVE_CURRENT_TENSORBOARD_LOGS + '_' +
-                                                  str(input_shape_image), update_freq='batch')
-    with LogCallback(logs_save_path=SAVE_CURRENT_LOGS + '_' + str(INPUT_SHAPE_IMAGE)) as call_back:
+    tensor_board = tf.keras.callbacks.TensorBoard(save_current_logs, update_freq='batch')
+    with LogCallback(logs_save_path=save_current_logs, model_save_path=save_current_model,
+                     input_shape=input_shape_image) as call_back:
         model.fit_generator(generator=train_data_gen, validation_data=test_data_gen, validation_freq=1,
                             validation_steps=len(test_data_gen), epochs=EPOCHS, workers=8,
                             callbacks=[early, model_checkpoint_callback, tensor_board, call_back])
 
 
-if __name__ == '__main__':
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    devices = tf.config.experimental.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(devices[0], True)
+def parse_args() -> argparse.Namespace:
+    """
+    Parsing command line arguments with argparse.
+    """
+    parser = argparse.ArgumentParser('script for model testing.')
+    parser.add_argument('--train', default=False, action='store_true',
+                        help='if you use this flag, then the training will be performed automatically with input shape'
+                             '(256*256*3))')
+    parser.add_argument('--train_dif_shape', default=False, action='store_true',
+                        help='if you use this flag, then the training will be performed automatically with a different'
+                             'input shape from (256*256*3) before (512*512*3)')
+    parser.add_argument('--data_path', type=str, default='data', help='path to Dataset where there is a json file')
+    parser.add_argument('--models_data', type=str, default='models_data', help='path for saving logs and models')
+    parser.add_argument('--gpu', type=str, default=0, help='which gpu to use in')
+    return parser.parse_args()
 
-    if parse_args().train is True:
-        train(JSON_FILE_PATH, input_shape_image=INPUT_SHAPE_IMAGE)
+
+if __name__ == '__main__':
+    args = parse_args()
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    DEVICES_TO_USE = [args.gpu]
+    devices = tf.config.experimental.list_physical_devices('GPU')
+    devices = [devices[i] for i in DEVICES_TO_USE]
+    # tf.config.set_visible_devices(devices)
+    for device in devices:
+        tf.config.experimental.set_memory_growth(device, True)
+
+    if args.train is True:
+        train(dataset_path_json=os.path.join(args.data_path, JSON_FILE_PATH), input_shape_image=INPUT_SHAPE_IMAGE)
 
     if parse_args().train_dif_shape is True:
         for i in range(5):
             input_shape = [(INPUT_SHAPE_IMAGE[0] + (64 * i), INPUT_SHAPE_IMAGE[1] +
                             (64 * i), 3) for i in range(0, 5, 1)]
-            train(dataset_path_json=JSON_FILE_PATH, input_shape_image=input_shape[i])
+            train(dataset_path_json=os.path.join(args.data_path, JSON_FILE_PATH), input_shape_image=input_shape[i])
