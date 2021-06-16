41364c3af2d45fef88af7ddabcfe0b1f85996de2
андрей китенко <genialniy.kitenko@gmail.com>
2021-06-16 19:24:47 +0300

    change

--- train.py
+++ train.py
@@ -14,11 +14,11 @@ from src import DataGenerator
 from src import LogCallback
 
 
-def train(dataset_path_json: str, input_shape_image: Tuple[int, int, int] = INPUT_SHAPE_IMAGE) -> None:
+def train(data_path: str, input_shape_image: Tuple[int, int, int] = INPUT_SHAPE_IMAGE) -> None:
     """
     Training to classify generated images.
 
-    :param dataset_path_json: path to json file.
+    :param data_path: data path.
     :param input_shape_image: this is image shape (height, width, channels).
     """
     date_time_for_save = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
@@ -32,8 +32,10 @@ def train(dataset_path_json: str, input_shape_image: Tuple[int, int, int] = INPU
     for p in [save_path, save_current_model, save_current_logs]:
         os.makedirs(p, exist_ok=True)
 
-    train_data_gen = DataGenerator(json_path=dataset_path_json, is_train=True, image_shape=input_shape_image)
-    test_data_gen = DataGenerator(json_path=dataset_path_json, is_train=False, image_shape=input_shape_image)
+    train_data_gen = DataGenerator(data_path=data_path, json_name=JSON_FILE_PATH, is_train=True,
+                                   image_shape=input_shape_image)
+    test_data_gen = DataGenerator(data_path=data_path, json_name=JSON_FILE_PATH, is_train=False,
+                                  image_shape=input_shape_image)
 
     model = build_model(image_shape=input_shape_image)
     model.compile(optimizer=tf.keras.optimizers.Adam(LEARNING_RATE),
@@ -42,7 +44,7 @@ def train(dataset_path_json: str, input_shape_image: Tuple[int, int, int] = INPU
                            sm.metrics.f1_score]
                   )
     model.summary()
-    early = EarlyStopping(monitor='loss', min_delta=0, patience=10, verbose=1, mode='auto')
+    early = EarlyStopping(monitor='loss', min_delta=0, patience=15, verbose=1, mode='auto')
     checkpoint_filepath = os.path.join(save_current_model, MODEL_NAME + BACKBONE + '.h5')
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
@@ -63,7 +65,7 @@ def parse_args() -> argparse.Namespace:
     """
     Parsing command line arguments with argparse.
     """
-    parser = argparse.ArgumentParser('script for model train.')
+    parser = argparse.ArgumentParser('script for model testing.')
     parser.add_argument('--train', default=False, action='store_true',
                         help='if you use this flag, then the training will be performed automatically with input shape'
                              '(256*256*3))')
@@ -72,25 +74,29 @@ def parse_args() -> argparse.Namespace:
                              'input shape from (256*256*3) before (512*512*3)')
     parser.add_argument('--data_path', type=str, default='data', help='path to Dataset where there is a json file')
     parser.add_argument('--models_data', type=str, default='models_data', help='path for saving logs and models')
-    parser.add_argument('--gpu', type=str, default=0, help='which gpu to use in')
+    parser.add_argument('--gpu', type=int, default=0, help='which gpu to use in')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
+    # # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    # # def func(gpu: Optional[int]) -> None:
+    # # GPU
+    # devices = tf.config.experimental.list_physical_devices('GPU')
+    # tf.config.set_visible_devices(devices[args.gpu], 'GPU')
+    # tf.config.experimental.set_memory_growth(devices[args.gpu], True)
+    # # NO GPU
+    # tf.config.set_visible_devices([], 'GPU')
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     DEVICES_TO_USE = [args.gpu]
     devices = tf.config.experimental.list_physical_devices('GPU')
     devices = [devices[i] for i in DEVICES_TO_USE]
-    # tf.config.set_visible_devices(devices)
-    for device in devices:
-        tf.config.experimental.set_memory_growth(device, True)
-
     if args.train is True:
-        train(dataset_path_json=os.path.join(args.data_path, JSON_FILE_PATH), input_shape_image=INPUT_SHAPE_IMAGE)
+        train(data_path=args.data_path, input_shape_image=INPUT_SHAPE_IMAGE)
 
     if parse_args().train_dif_shape is True:
         for i in range(5):
             input_shape = [(INPUT_SHAPE_IMAGE[0] + (64 * i), INPUT_SHAPE_IMAGE[1] +
                             (64 * i), 3) for i in range(0, 5, 1)]
-            train(dataset_path_json=os.path.join(args.data_path, JSON_FILE_PATH), input_shape_image=input_shape[i])
+            train(data_path=args.data_path, input_shape_image=input_shape[i])
