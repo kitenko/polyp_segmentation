a73cc49620142cabad104c9bbc997fd7bd083f90
андрей китенко <genialniy.kitenko@gmail.com>
2021-06-16 19:24:18 +0300

    change

--- src/logcallback.py
+++ src/logcallback.py
@@ -5,14 +5,15 @@ from typing import Dict, Tuple
 from tensorflow import keras
 
 from config import (SAVE_MODEL_EVERY_EPOCH, BATCH_SIZE, LEARNING_RATE, BACKBONE, ENCODER_WEIGHTS, OUTPUT_ACTIVATION,
-                    MODEL_NAME, AUGMENTATION_DATA)
+                    MODEL_NAME, AUGMENTATION_DATA, LAYER_ACTIVATION)
 
 
 class LogCallback(keras.callbacks.Callback):
     def __init__(self, model_save_path: str, logs_save_path: str, input_shape: Tuple, model_name: str = MODEL_NAME,
                  batch_size: int = BATCH_SIZE, backbone: str = BACKBONE, learning_rate: float = LEARNING_RATE,
                  encoder_weights: str = ENCODER_WEIGHTS, save_model_every_era: bool = SAVE_MODEL_EVERY_EPOCH,
-                 augmentation_data: bool = AUGMENTATION_DATA, output_activation: str = OUTPUT_ACTIVATION) -> None:
+                 augmentation_data: bool = AUGMENTATION_DATA, output_activation: str = OUTPUT_ACTIVATION,
+                 layer_activation: str = LAYER_ACTIVATION) -> None:
         """
         Logging all training metrics to a json file and saving the model every epoch.
 
@@ -28,33 +29,36 @@ class LogCallback(keras.callbacks.Callback):
         :param encoder_weights: whether imagenet weights are used or not.
         :param augmentation_data: whether augmentation is used or not.
         :param output_activation: this is output activation.
+        :param layer_activation: this is layer activation.
 
         """
         super().__init__()
         self.model_save_path = model_save_path
         self.log_file = os.path.join(logs_save_path, 'train_logs.json')
-        self.logs = {'epochs': []}
         self.save_model_every_epoch = save_model_every_era
-        self.input_shape = input_shape
-        self.model_name = model_name
-        self.batch_size = batch_size
-        self.backbone = backbone
-        self.learning_rate = learning_rate
-        self.encoder_weights = encoder_weights
-        self.augmentation_data = augmentation_data
-        self.output_activation = output_activation
+        self.logs = {'epochs': [],
+                     'Model_name': model_name,
+                     'Backbone': backbone,
+                     'Input_shape': input_shape,
+                     'Batch_size': batch_size,
+                     'Learning_rate': learning_rate,
+                     'Encoder_weights': encoder_weights,
+                     'Augmentation_data': augmentation_data,
+                     'Output_activation': output_activation,
+                     'Layer_activation': layer_activation
+                     }
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.save_logs()
-        self.neural_network_parameters()
         if self.save_model_every_epoch:
             self.model.save(os.path.join(self.model_save_path, 'last.h5'))
+        self.neural_network_parameters()
 
     def save_logs(self) -> None:
-        with open(self.log_file, 'a') as file:
+        with open(self.log_file, 'w') as file:
             json.dump(self.logs, file, indent=4)
 
     def on_epoch_end(self, epoch: int, logs: Dict[str, float] = None) -> None:
@@ -75,17 +79,3 @@ class LogCallback(keras.callbacks.Callback):
         self.save_logs()
         if self.save_model_every_epoch:
             self.model.save(os.path.join(self.model_save_path, '{:03d}_epoch.h5'.format(1 + epoch)))
-
-    def neural_network_parameters(self):
-        parameters = {'Model_name': self.model_name,
-                      'Backbone': self.backbone,
-                      'Input_shape': self.input_shape,
-                      'Batch_size': self.batch_size,
-                      'Learning_rate': self.learning_rate,
-                      'Encoder_weights': self.encoder_weights,
-                      'Augmentation_data': self.augmentation_data,
-                      'Output_activation': self.output_activation
-                     }
-
-        with open(self.log_file, 'a') as file:
-            json.dump(parameters, file, indent=4)
