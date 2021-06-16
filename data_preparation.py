0b18907715735a429ed2884f6cebc593b57ca803
андрей китенко <genialniy.kitenko@gmail.com>
2021-06-16 19:24:04 +0300

    change

--- data_preparation.py
+++ data_preparation.py
@@ -5,49 +5,56 @@ import argparse
 
 import cv2
 
-from config import JSON_FILE_PATH, IMAGES_PATH, MASKS_PATH, PROPORTION_TEST_IMAGES
+from config import JSON_FILE_PATH, PROPORTION_TEST_IMAGES
 
 
-def prepare_data(masks_path: str = MASKS_PATH, proportion_test_images: float = PROPORTION_TEST_IMAGES,
-                 json_file_path: str = JSON_FILE_PATH, images_path: str = IMAGES_PATH) -> None:
+def prepare_data(data_path: str, proportion_test_images: float = PROPORTION_TEST_IMAGES) -> None:
     """
-
-    :param masks_path: this is pass for masks files.
     :param proportion_test_images: proportion of test images.
-    :param json_file_path: path to save json file.
-    :param images_path: this is path for image files.
+    :param data_path: path data.
     """
+
+    path = []
+    for root, dirs, _ in os.walk(data_path):
+        for dir in dirs:
+            if dir == 'images':
+                path.append(os.path.join(root, dir))
+            elif dir == 'masks':
+                path.append(os.path.join(root, dir))
 
     # reading and shuffling files
-    count_images = os.listdir(images_path)
-    shuffle_images = random.sample(count_images, len(count_images))
+    images = []
+    for image_path in path:
+        g = image_path.split('/')
+        if 'images' in g:
+            for mask_path in os.listdir(image_path):
+                images.append(os.path.join(image_path, mask_path))
+
+    shuffle_images = random.sample(images, len(images))
 
     # create dictionary
     train_test_json = {'train': [], 'test': []}
 
-    # checking masks file in folder
-    def find(name, path):
-        for root, dirs, files in os.walk(path):
-            if name in files:
-                return os.path.join(name)
-
     # filling in dictionary for json file
-    for j, i in enumerate(shuffle_images):
+    for j, image_path in enumerate(shuffle_images):
         try:
-            masks = find(i.rsplit(".", 1)[0] + '.jpg', masks_path)
-            img_dict = {'image_path': os.path.join(images_path, i), 'mask_path': os.path.join(masks_path, masks)}
-            if cv2.imread(os.path.join(images_path, i)) is None:
-                print('broken image')
+            mask_path = image_path.replace('images', 'masks')
+            img_dict = {'image_path': image_path, 'mask_path': mask_path}
+            if cv2.imread(image_path) is None:
+                print('broken image: ' + image_path)
+                continue
+            elif cv2.imread(mask_path) is None:
+                print('broken image: ' + mask_path)
                 continue
             elif j < len(shuffle_images) * proportion_test_images:
                 train_test_json['test'].append(img_dict)
             else:
                 train_test_json['train'].append(img_dict)
         except KeyError:
-            print(' no mask for ', i)
+            print(' no mask for ', image_path)
 
     # write json file
-    with open(json_file_path, 'w') as f:
+    with open(os.path.join(data_path, JSON_FILE_PATH), 'w') as f:
         json.dump(train_test_json, f, indent=4)
 
 
@@ -56,13 +63,11 @@ def parse_args() -> argparse.Namespace:
     Parsing command line arguments with argparse.
     """
     parser = argparse.ArgumentParser('script for model testing.')
-    parser.add_argument('--data_path', nargs='?', default=None, const='data', help='path to Dataset')
+    parser.add_argument('-p', '--data_path', type=str, default='data', help='path to Dataset')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
 
-    prepare_data(json_file_path=os.path.join(args.data_path, JSON_FILE_PATH),
-                 images_path=os.path.join(args.data_path, IMAGES_PATH),
-                 masks_path=os.path.join(args.data_path, MASKS_PATH))
+    prepare_data(data_path=args.data_path)
