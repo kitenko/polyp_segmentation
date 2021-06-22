37f844e58c9b89daf5bbcad80ac0fa0d919fb3bb
андрей китенко <genialniy.kitenko@gmail.com>
2021-06-22 17:29:58 +0300

    deleted show() deleted parse_args()

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,12 +1,10 @@
 import os
 import json
-import argparse
 from typing import Tuple
 
 import cv2
 import numpy as np
 import albumentations as A
-import matplotlib.pyplot as plt
 from tensorflow import keras
 
 
@@ -19,7 +17,6 @@ class DataGenerator(keras.utils.Sequence):
                  num_classes: int = NUMBER_CLASSES, augmentation_data: bool = AUGMENTATION_DATA) -> None:
         """
         Data generator for the task of semantic segmentation.
-
 
         :param batch_size: number of images in one batch.
         :param image_shape: this is image shape (height, width, channels).
@@ -71,8 +68,7 @@ class DataGenerator(keras.utils.Sequence):
         masks = np.zeros((self.batch_size, self.image_shape[1], self.image_shape[0], self.num_classes))
         for i, image_dict in enumerate(batch):
             img = cv2.imread(image_dict['image_path'])
-            if os.path.basename(image_dict['image_path']).split('.')[-1] == 'png' or 'jpg':
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             mask_image = cv2.imread(image_dict['mask_path'], 0)
             augmented = self.aug(image=img, mask=mask_image)
             img = augmented['image']
@@ -82,31 +78,6 @@ class DataGenerator(keras.utils.Sequence):
             masks[i, :, :, -1] = np.where(mask_image == 0, 1, 0)    # background
         images = image_normalization(images)
         return images, masks
-
-    def show(self):
-        """
-        This function shows image and masks.
-        """
-        for i in range(len(self)):
-            batch = self[i]
-
-            images, masks = batch[0], batch[1]
-            fontsize = 8
-            for i, j in enumerate(images):
-                mask_background = masks[i, :, :, -1]
-                mask_object = masks[i, :, :, 0]
-                plt.figure(figsize=[10, 10])
-                f, ax = plt.subplots(3, 1)
-                ax[0].imshow(j)
-                ax[0].set_title('Original image', fontsize=fontsize)
-                ax[1].imshow(mask_object)
-                ax[1].set_title('Mask dog or cat', fontsize=fontsize)
-                ax[2].imshow(mask_background)
-                ax[2].set_title('Mask background', fontsize=fontsize)
-                if plt.waitforbuttonpress(0):
-                    plt.close('all')
-                    raise SystemExit
-                plt.close()
 
     def augmentation_images(self, augm: bool = False) -> A.Compose:
         """
@@ -136,18 +107,3 @@ def image_normalization(image: np.ndarray) -> np.ndarray:
     :return: normalized image.
     """
     return image / 255.0
-
-
-def parse_args() -> argparse.Namespace:
-    """
-    Parsing command line arguments with argparse.
-    """
-    parser = argparse.ArgumentParser('script for model testing.')
-    parser.add_argument('-p', '--data_path', type=str, default='data', help='path to Dataset')
-    return parser.parse_args()
-
-
-if __name__ == '__main__':
-    args = parse_args()
-    x = DataGenerator(data_path=args.data_path)
-    x.show()
